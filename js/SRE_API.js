6b9123f0752b602c170721882b78e465d7f362ba
Rubén de Celis Hernández <rubendecelis@ingran.es>
2016-12-14 13:01:10 +0100

    Minor code style fixes

--- js/SRE_API.js
+++ js/SRE_API.js
@@ -3,24 +3,24 @@
 function include(file)
 {
 
-  var script  = document.createElement('script');
+  var script  = document.createElement("script");
   script.src  = file;
-  script.type = 'text/javascript';
+  script.type = "text/javascript";
   script.defer = true;
 
-  document.getElementsByTagName('head').item(0).appendChild(script);
+  document.getElementsByTagName("head").item(0).appendChild(script);
 
 }
 
-/* include any js files here */
-include('js/common.js');
-include('js/encode.js');
-include('js/decode.js');
-include('js/utf8.js');
+/* Include any js files here */
+include("js/common.js");
+include("js/encode.js");
+include("js/decode.js");
+include("js/utf8.js");
 
-function convertToURL(str){ return encode(str) };
-function convertToString(url){ return decode (url)};
+function convertToURL(str){ return encode(str); };
+function convertToString(url){ return decode (url); };
 
-function convertToURLWithUTF8(str){ return encode(utf8_encode(str)) };
-function convertToStringWithUTF8(url){ return utf8_decode(decode (url))};
+function convertToURLWithUTF8(str){ return encode(utf8_encode(str)); };
+function convertToStringWithUTF8(url){ return utf8_decode(decode (url)); };
 
--- js/utf8.js
+++ js/utf8.js
@@ -15,60 +15,60 @@ function utf8_encode (argString) { // eslint-disable-line camelcase
   //   returns 1: 'Kevin van Zonneveld'
 
   if (argString === null || typeof argString === 'undefined') {
-    return ''
+    return '';
   }
 
   // .replace(/\r\n/g, "\n").replace(/\r/g, "\n");
-  var string = (argString + '')
-  var utftext = ''
-  var start
-  var end
-  var stringl = 0
+  var string = (argString + '');
+  var utftext = '';
+  var start;
+  var end;
+  var stringl = 0;
 
-  start = end = 0
-  stringl = string.length
+  start = end = 0;
+  stringl = string.length;
   for (var n = 0; n < stringl; n++) {
-    var c1 = string.charCodeAt(n)
-    var enc = null
+    var c1 = string.charCodeAt(n);
+    var enc = null;
 
     if (c1 < 128) {
-      end++
+      end++;
     } else if (c1 > 127 && c1 < 2048) {
       enc = String.fromCharCode(
         (c1 >> 6) | 192, (c1 & 63) | 128
-      )
+      );
     } else if ((c1 & 0xF800) !== 0xD800) {
       enc = String.fromCharCode(
         (c1 >> 12) | 224, ((c1 >> 6) & 63) | 128, (c1 & 63) | 128
-      )
+      );
     } else {
       // surrogate pairs
       if ((c1 & 0xFC00) !== 0xD800) {
-        throw new RangeError('Unmatched trail surrogate at ' + n)
+        throw new RangeError('Unmatched trail surrogate at ' + n);
       }
       var c2 = string.charCodeAt(++n)
       if ((c2 & 0xFC00) !== 0xDC00) {
-        throw new RangeError('Unmatched lead surrogate at ' + (n - 1))
+        throw new RangeError('Unmatched lead surrogate at ' + (n - 1));
       }
       c1 = ((c1 & 0x3FF) << 10) + (c2 & 0x3FF) + 0x10000
       enc = String.fromCharCode(
         (c1 >> 18) | 240, ((c1 >> 12) & 63) | 128, ((c1 >> 6) & 63) | 128, (c1 & 63) | 128
-      )
+      );
     }
     if (enc !== null) {
       if (end > start) {
-        utftext += string.slice(start, end)
+        utftext += string.slice(start, end);
       }
-      utftext += enc
-      start = end = n + 1
+      utftext += enc;
+      start = end = n + 1;
     }
   }
 
   if (end > start) {
-    utftext += string.slice(start, stringl)
+    utftext += string.slice(start, stringl);
   }
 
-  return utftext
+  return utftext;
 }
 
 function utf8_decode (strData) { // eslint-disable-line camelcase
@@ -86,46 +86,46 @@ function utf8_decode (strData) { // eslint-disable-line camelcase
   //   example 1: utf8_decode('Kevin van Zonneveld')
   //   returns 1: 'Kevin van Zonneveld'
 
-  var tmpArr = []
-  var i = 0
-  var c1 = 0
-  var seqlen = 0
+  var tmpArr = [];
+  var i = 0;
+  var c1 = 0;
+  var seqlen = 0;
 
-  strData += ''
+  strData += '';
 
   while (i < strData.length) {
-    c1 = strData.charCodeAt(i) & 0xFF
-    seqlen = 0
+    c1 = strData.charCodeAt(i) & 0xFF;
+    seqlen = 0;
 
     // http://en.wikipedia.org/wiki/UTF-8#Codepage_layout
     if (c1 <= 0xBF) {
-      c1 = (c1 & 0x7F)
-      seqlen = 1
+      c1 = (c1 & 0x7F);
+      seqlen = 1;
     } else if (c1 <= 0xDF) {
-      c1 = (c1 & 0x1F)
-      seqlen = 2
+      c1 = (c1 & 0x1F);
+      seqlen = 2;
     } else if (c1 <= 0xEF) {
-      c1 = (c1 & 0x0F)
-      seqlen = 3
+      c1 = (c1 & 0x0F);
+      seqlen = 3;
     } else {
-      c1 = (c1 & 0x07)
-      seqlen = 4
+      c1 = (c1 & 0x07);
+      seqlen = 4;
     }
 
     for (var ai = 1; ai < seqlen; ++ai) {
-      c1 = ((c1 << 0x06) | (strData.charCodeAt(ai + i) & 0x3F))
+      c1 = ((c1 << 0x06) | (strData.charCodeAt(ai + i) & 0x3F));
     }
 
     if (seqlen === 4) {
-      c1 -= 0x10000
-      tmpArr.push(String.fromCharCode(0xD800 | ((c1 >> 10) & 0x3FF)))
-      tmpArr.push(String.fromCharCode(0xDC00 | (c1 & 0x3FF)))
+      c1 -= 0x10000;
+      tmpArr.push(String.fromCharCode(0xD800 | ((c1 >> 10) & 0x3FF)));
+      tmpArr.push(String.fromCharCode(0xDC00 | (c1 & 0x3FF)));
     } else {
-      tmpArr.push(String.fromCharCode(c1))
+      tmpArr.push(String.fromCharCode(c1));
     }
 
-    i += seqlen
+    i += seqlen;
   }
 
-  return tmpArr.join('')
+  return tmpArr.join('');
 }
